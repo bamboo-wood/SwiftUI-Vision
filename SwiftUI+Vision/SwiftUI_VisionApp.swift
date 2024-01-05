66e9e59c8b5ac0452936544db6f828eb4efe4339
bamboo-wood <cantate.1009.0417@gmail.com>
2024-01-05 16:02:08 +0900

    Initial Commit

---
+++ SwiftUI+Vision/SwiftUI_VisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_VisionApp.swift
+//  SwiftUI+Vision
+//
+//  Created by 佐々木一樹 on 2024/01/05.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_VisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
